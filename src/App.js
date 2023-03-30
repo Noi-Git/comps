c27d72be59fa3cfaf6465797c450fc517526375d
Noi-Git <noi.patsin@gmail.com>
2023-03-29 21:34:02 -0400

    setup dropdown component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import './App.css'
-import Accordion from './components/Accordion'
+import Dropdown from './components/Dropdown'
 
 function App() {
-  const items = [
-    {
-      id: 1,
-      label: 'Can I use React on a project?',
-      content:
-        'You can use React on any project you want. You need to also understand redux',
-    },
-    {
-      id: 2,
-      label: 'Can I use Javascript on a project?',
-      content:
-        'You can use Javascript on any project you want. But make sure you understand how Javascript works',
-    },
-    {
-      id: 3,
-      label: 'Can I use CSS on a project?',
-      content:
-        'You can use CSS on any project you want. CSS seems like easy but it is really complicated',
-    },
+  const options = [
+    { label: 'Red', value: 'red' },
+    { label: 'Green', value: 'green' },
+    { label: 'Blue', value: 'blue' },
   ]
 
-  return <Accordion items={items} />
+  return <Dropdown options={options} />
 }
 
 export default App
---
+++ src/pages/AccordionPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './App.css'
+import Accordion from '../components/Accordion'
+
+const AccordionPage = () => {
+  const items = [
+    {
+      id: 1,
+      label: 'Can I use React on a project?',
+      content:
+        'You can use React on any project you want. You need to also understand redux',
+    },
+    {
+      id: 2,
+      label: 'Can I use Javascript on a project?',
+      content:
+        'You can use Javascript on any project you want. But make sure you understand how Javascript works',
+    },
+    {
+      id: 3,
+      label: 'Can I use CSS on a project?',
+      content:
+        'You can use CSS on any project you want. CSS seems like easy but it is really complicated',
+    },
+  ]
+
+  return <Accordion items={items} />
+}
+
+export default AccordionPage
