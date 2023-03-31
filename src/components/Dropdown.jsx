d54a4a67e519585ec31aad79bd70d1aa7f61b4cd
Noi-Git <noi.patsin@gmail.com>
2023-03-30 23:13:33 -0400

    update style name

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -19,7 +19,7 @@ const Dropdown = ({ options, value, onChange }) => {
   const renderedOptions = options.map((option) => {
     return (
       <div
-        className='hover:bg-sky-100 rounded-lg cursor-pointer p-1'
+        className='hover:bg-sky-100 rounded cursor-pointer p-1'
         onClick={() => handleOptionClick(option)}
         key={option.value}
       >
@@ -41,14 +41,14 @@ const Dropdown = ({ options, value, onChange }) => {
   return (
     <div className='w-48 relative'>
       <div
-        className='flex justify-between items-center cursor-pointer border rounded-lg p-3 shadow bg-white w-full'
+        className='flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full'
         onClick={handleClick}
       >
         {value?.label || 'Select...'}
         <GoChevronDown className='text-lg' />
       </div>
       {isOpen && (
-        <div className='absolute top-full border rounded-lg p-3 shadow bg-white w-full'>
+        <div className='absolute top-full border rounded-b p-3 shadow bg-white w-full'>
           {renderedOptions}
         </div>
       )}
