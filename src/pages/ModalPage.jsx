a00296f49bde6c259c085f2c3d8d686646ff5224
Noi-Git <noi.patsin@gmail.com>
2023-04-01 11:59:10 -0400

    use Button component created earlier in ModalPage

--- src/pages/ModalPage.jsx
+++ src/pages/ModalPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Modal from '../components/Modal'
+import Button from '../components/Button'
 
 const ModalPage = () => {
   return (
     <div>
+      <Button primary>Open Modal</Button>
       <Modal />
     </div>
   )
