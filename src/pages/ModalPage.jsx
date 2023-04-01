dc5ba72923e8b714ba15882e776ad7d2b0f40402
Noi-Git <noi.patsin@gmail.com>
2023-04-01 12:02:04 -0400

    setup modal state

--- src/pages/ModalPage.jsx
+++ src/pages/ModalPage.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { useState } from 'react'
 import Modal from '../components/Modal'
 import Button from '../components/Button'
 
 const ModalPage = () => {
+  const [showModal, setShowModal] = useState(false)
+
+  const handleClick = () => {
+    setShowModal(true)
+  }
+
   return (
     <div>
-      <Button primary>Open Modal</Button>
+      <Button primary onClick={handleClick}>
+        Open Modal
+      </Button>
       <Modal />
     </div>
   )
