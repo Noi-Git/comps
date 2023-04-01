ce658e078407b83efcdb9266f5e78bc22cf62930
Noi-Git <noi.patsin@gmail.com>
2023-03-31 22:42:59 -0400

    implement allow user to open page on new tab

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -5,12 +5,22 @@ const Link = ({ to, children }) => {
   const { navigate } = useContext(NavigationContext)
 
   const handleClick = (event) => {
+    // console.log(event) //look in the console -- ctrlKey and metaKey
+    // check if user hold down command key on mac ot ctrl key on window -- open the new page
+    if (event.metaKey || event.ctrlKey) {
+      return
+    }
+    // if the above is not true -- preventDefault
     event.preventDefault()
 
     navigate(to)
   }
 
-  return <a onClick={handleClick}>{children}</a>
+  return (
+    <a href={to} onClick={handleClick}>
+      {children}
+    </a>
+  )
 }
 
 export default Link
