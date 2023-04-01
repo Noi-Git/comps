b7d60bb574ed2268c6bf81a286e0205f22b792a6
Noi-Git <noi.patsin@gmail.com>
2023-03-31 21:53:19 -0400

    example of how to use navigate function

--- src/context/navigation.jsx
+++ src/context/navigation.jsx
@@ -24,6 +24,10 @@ const NavigationProvider = ({ children }) => {
 
   return (
     <NavigationContext.Provider value={{}}>
+      <div>
+        <button onClick={() => navigate('/accordion')}>Go to accordion</button>
+        <button onClick={() => navigate('/dropdown')}>Go to dropdown</button>
+      </div>
       {currentPath}
       {children}
     </NavigationContext.Provider>
