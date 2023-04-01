959862783d78a405312ebdd54ee136fcda945084
Noi-Git <noi.patsin@gmail.com>
2023-03-31 22:35:09 -0400

    implementing route to the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import './App.css'
 import Link from './components/Link'
+import Route from './components/Route'
+import AccordionPage from './pages/AccordionPage'
+import DropdownPage from './pages/DropdownPage'
 
 function App() {
   return (
     <div>
-      <Link to='/accordion'>Go to accordion</Link>
-      <Link to='/dropdown'>Go to dropdown</Link>
+      <button>
+        <Link to='/accordion'>Go to accordion</Link>
+      </button>
+      <button>
+        <Link to='/dropdown'>Go to dropdown</Link>
+      </button>
+      <div>
+        <Route path='/accordion'>
+          <AccordionPage />
+        </Route>
+        <Route path='/dropdown'>
+          <DropdownPage />
+        </Route>
+      </div>
     </div>
   )
 }
--- src/components/Route.jsx
+++ src/components/Route.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import { useContext } from 'react'
 import NavigationContext from '../context/navigation'
 
 const Route = ({ path, children }) => {
