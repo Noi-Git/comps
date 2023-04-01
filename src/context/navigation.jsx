60dc2dd35637fbb53599c352cc38e921d5a68e00
Noi-Git <noi.patsin@gmail.com>
2023-03-31 21:32:15 -0400

    set default to current pathname

--- src/context/navigation.jsx
+++ src/context/navigation.jsx
@@ -1,8 +1,9 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 const NavigationContext = createContext()
 
 const NavigationProvider = ({ children }) => {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
   return (
     <NavigationContext.Provider value={{}}>
       {children}
