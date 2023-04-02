4e471fb70343a44f2f64b03bc487618649e363dc
Noi-Git <noi.patsin@gmail.com>
2023-04-01 22:34:12 -0400

    style color colunm

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -4,7 +4,7 @@ import Table from '../components/Table'
 const TablePage = () => {
   const data = [
     { name: 'Orange', color: 'bg-orange-500', score: 5 },
-    { name: 'Apple', color: 'bg-apple-500', score: 3 },
+    { name: 'Apple', color: 'bg-red-500', score: 3 },
     { name: 'Banana', color: 'bg-yellow-500', score: 1 },
     { name: 'Lime', color: 'bg-green-500', score: 4 },
   ]
