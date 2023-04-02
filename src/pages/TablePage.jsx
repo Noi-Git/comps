c006b82e2bb9c79c5475d25fb40bff3450872d94
Noi-Git <noi.patsin@gmail.com>
2023-04-02 15:09:16 -0400

    create reusable table - add config array to parent

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -9,9 +9,11 @@ const TablePage = () => {
     { name: 'Lime', color: 'bg-green-500', score: 4 },
   ]
 
+  const config = [{ label: 'Name' }, { label: 'Color' }, { label: 'Score' }]
+
   return (
     <div>
-      <Table data={data} />
+      <Table data={data} config={config} />
     </div>
   )
 }
