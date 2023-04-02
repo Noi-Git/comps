274b3f26be03ab7032989218aa79faa7bd94da9e
Noi-Git <noi.patsin@gmail.com>
2023-04-01 22:30:54 -0400

    style table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 const Table = ({ data }) => {
   const renderedRows = data.map((fruit) => {
     return (
-      <tr key={fruit.name}>
-        <td>{fruit.name}</td>
-        <td>{fruit.color}</td>
-        <td>{fruit.score}</td>
+      <tr className='border-b' key={fruit.name}>
+        <td className='p-3'>{fruit.name}</td>
+        <td className='p-3'>{fruit.color}</td>
+        <td className='p-3'>{fruit.score}</td>
       </tr>
     )
   })
 
   return (
-    <table>
+    <table className='table-zuto border-spacing-2'>
       <thead>
-        <tr>
+        <tr className='border-b-2'>
           <th>Fruit</th>
           <th>Color</th>
           <th>Score</th>
