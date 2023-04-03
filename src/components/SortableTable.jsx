e6b9ff1eb4a7a73af1dbf27a53cc8de24f62b676
Noi-Git <noi.patsin@gmail.com>
2023-04-02 22:16:44 -0400

    Sort 14: add style

--- src/components/SortableTable.jsx
+++ src/components/SortableTable.jsx
@@ -36,8 +36,11 @@ const SortableTable = (props) => {
     return {
       ...column,
       header: () => (
-        <th onClick={() => handleClick(column.label)}>
-          <div className='flex items-center'>
+        <th
+          className='cursor-pointer hover:bg-gray-100'
+          onClick={() => handleClick(column.label)}
+        >
+          <div className='flex items-center pr-2'>
             {getIcons(column.label, sortBy, sortOrder)}
             {column.label}
           </div>
