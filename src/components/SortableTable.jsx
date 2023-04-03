f726f3764620a90496f261b37efc8d1c7e8d4ba3
Noi-Git <noi.patsin@gmail.com>
2023-04-02 20:16:28 -0400

    Sort 3: map through config and add header option

--- src/components/SortableTable.jsx
+++ src/components/SortableTable.jsx
@@ -1,7 +1,25 @@
 import Table from './Table'
 
 const SortableTable = (props) => {
-  return <Table {...props} />
+  /* we are not modifying the props 
+     - we only map over the config
+     - if find the object that have SortValue 
+       --- add header with sort function
+  */
+  const { config } = props
+
+  const updatedConfig = config.map((column) => {
+    if (!column.sortValue) {
+      return column
+    }
+
+    return {
+      ...column,
+      header: () => <th>{column.label} IS SORTABLE</th>,
+    }
+  })
+
+  return <Table {...props} config={updatedConfig} />
 }
 
 export default SortableTable
