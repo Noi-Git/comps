b2012171a3235d45ea043311dc7e14eb88540273
Noi-Git <noi.patsin@gmail.com>
2023-04-03 20:47:58 -0400

    CusH 4 call cusH and destructure the properties

--- src/pages/DemoCounterPage.jsx
+++ src/pages/DemoCounterPage.jsx
@@ -19,6 +19,8 @@ const useSomething = () => {
 }
 
 const DemoCounterPage = ({ initialCount }) => {
+  const { count, handleClick } = useSomething()
+
   return (
     <div>
       <h1>Count is {count}</h1>
